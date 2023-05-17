5ddd5cb8bb93dc0db74c7572369e5d31ae218ce1
Guilherme <guisou1902@gmail.com>
2023-05-17 11:21:16 -0300

    feat

--- src/components/tasks/Tasks.jsx
+++ src/components/tasks/Tasks.jsx
@@ -11,11 +11,28 @@ const Tasks = ({taskList}) => {
 		});
 	};
 
+	function handleItemClick(index) {
+		const taskListString = localStorage.getItem("taskList");
+		const taskList = JSON.parse(taskListString);
+	  
+		taskList.splice(index, 1);
+	  
+		const updatedTaskListString = JSON.stringify(taskList);
+		localStorage.setItem("taskList", updatedTaskListString);
+		
+		setTimeout(() => {
+			window.location.reload()
+		}, 500)
+	}	  
+
   	return (
 		<div className="tasks-container">
 			<ul className="tasks">
 				{taskList.map((task, index) => (
-					<li key={index} className={activated[index] ? "task activated" : "task"}  onClick={ () => toggleClass(index) } >
+					<li key={index} className={activated[index] ? "task activated" : "task"}  onClick={ () => {
+						toggleClass(index)
+						handleItemClick(index)
+					}}>
 						<button className="btn-task"></button>
 						<span>{task}</span>
 					</li>
